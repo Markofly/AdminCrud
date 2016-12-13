40022fe4436fbc0a9ffb6f4c5c08d0a31bf96e25
Marko Murumaa <marko.murumaa@gmail.com>
2016-12-13 21:34:28 +0200

    replaced baseController with trait

--- src/AdminCrudTrait.php
+++ src/AdminCrudTrait.php
@@ -1,16 +1,9 @@
 <?php
-namespace Markofly\AdminCrud\Controllers;
 
-use Illuminate\Http\Request;
-use Markofly\AdminCrud\AdminCrud;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+namespace Markofly\AdminCrud;
 
-class baseCrudController extends BaseController
+trait AdminCrudTrait
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     protected $adminCrud;
 
@@ -18,35 +11,25 @@ class baseCrudController extends BaseController
 
     public function __construct()
     {
+        $this->initializeAdminCrud();
+    }
+
+    public function initializeAdminCrud()
+    {
         $this->adminCrud = new AdminCrud('users');
         $this->pageTitle = 'Users';
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         return view('AdminCrud::default', ['form' => $this->adminCrud->getList(), 'pageTitle' => $this->pageTitle]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('AdminCrud::default', ['form' => $this->adminCrud->getCreateForm(), 'pageTitle' => $this->pageTitle]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
 
@@ -59,35 +42,16 @@ class baseCrudController extends BaseController
         return redirect()->to($this->adminCrud->getObject()->getShowRoute($item->id))->with('success', 'Item is successfully created!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         return view('AdminCrud::default', ['form' => $this->adminCrud->getView($id), 'pageTitle' => $this->pageTitle]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         return view('AdminCrud::default', ['form' => $this->adminCrud->getEditForm($id), 'pageTitle' => $this->pageTitle]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $rules = $this->adminCrud->getValidationRules();
@@ -99,12 +63,6 @@ class baseCrudController extends BaseController
         return redirect()->back()->with('success', 'Item is successfully saved!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $item = $this->adminCrud->getModel()->findOrFail($id);
@@ -113,4 +71,3 @@ class baseCrudController extends BaseController
         return redirect()->back()->with('success', 'Item is successfully deleted!');
     }
 }
-
